009bb08a6663fefe6bcdb9e3b5bb47ab746f8a7b
Luke <luke@madebymade.co.uk>
2015-10-22 20:32:16 +0100

    Add more complex example

--- spec/typed_array_spec.rb
+++ spec/typed_array_spec.rb
@@ -8,4 +8,9 @@ describe Radical::Typed::Array do
     subject { Radical::Typed::Array[String].new([10]) }
     it { is_expected.to include('10') }
   end
+
+  context 'when given another typed array type' do
+    subject { Radical::Typed::Array[Radical::Typed::Array[Integer]].new([['10']]) }
+    it { is_expected.to include(array_including(10)) }
+  end
 end
--- spec/typed_hash_spec.rb
+++ spec/typed_hash_spec.rb
@@ -8,4 +8,10 @@ describe Radical::Typed::Hash do
     subject { Radical::Typed::Hash[name: String].new(name: 10) }
     it { is_expected.to include(name: '10') }
   end
+
+  context 'when given another typed hash type' do
+    let(:address_type) { Radical::Typed::Hash[number: String] }
+    subject { Radical::Typed::Hash[address: address_type].new(address: { number: 10 }) }
+    it { is_expected.to include(address: a_hash_including(number: '10')) }
+  end
 end
