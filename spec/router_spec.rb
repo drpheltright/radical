ad8c5e1d000f40318b8bddff46a30a0fb6022437
Luke Morton <lukemorton.dev@gmail.com>
2015-10-26 23:39:24 +0000

    Tidy up spec

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -4,52 +4,40 @@ describe Radical::Router do
   let(:cart_schema) { Radical::Typed::Hash[line_items: line_items_schema] }
   let(:user_schema) { Radical::Typed::Hash[profile: profile_schema, cart: cart_schema] }
 
+  let(:user_profile_route) do
+    Radical::Route[:user, Radical::Arg[:id], user_schema].define do
+      def get(id)
+        { profile: { name: 'Luke' } }
+      end
+    end
+  end
+
+  let(:user_cart_route) do
+    Radical::Route[:user, Radical::Arg[:id], user_schema].define do
+      def get(id)
+        { cart: { line_items: [{ id: 1 }, { id: 2 }] } }
+      end
+    end
+  end
+
+  let(:post_route) do
+    Radical::Route[:post, Radical::Arg[:id], Radical::Typed::Hash[title: String]].define do
+      def get(id)
+        { title: 'Very good post' }
+      end
+    end
+  end
+
+  subject { Radical::Router.new(routes).route([[:get, :user, 1]])[:user][1] }
+
   context 'when single request matches single route' do
-    let(:route_1) do
-      Radical::Route[:user, Radical::Arg[:id], user_schema].define do
-        def get(id)
-          { profile: { name: 'Luke' } }
-        end
-      end
-    end
-
-    let(:route_2) do
-      Radical::Route[:post, Radical::Arg[:id], Radical::Typed::Hash[title: String]].define do
-        def get(id)
-          { title: 'Very good post' }
-        end
-      end
-    end
-
-    let(:routes) { [route_1, route_2].map(&:new) }
-
-    subject { Radical::Router.new(routes).route([[:get, :user, 1]])[:user][1] }
-
+    let(:routes) { [user_profile_route, post_route].map(&:new) }
     it { is_expected.to include(profile: a_hash_including(name: 'Luke')) }
     it { is_expected.to_not include(:post) }
   end
 
   context 'when single request matches two routes' do
-    let(:route_1) do
-      Radical::Route[:user, Radical::Arg[:id], user_schema].define do
-        def get(id)
-          { profile: { name: 'Luke' } }
-        end
-      end
-    end
-
-    let(:route_2) do
-      Radical::Route[:user, Radical::Arg[:id], user_schema].define do
-        def get(id)
-          { cart: { line_items: [{ id: 1 }, { id: 2 }] } }
-        end
-      end
-    end
-
-    let(:routes) { [route_1, route_2].map(&:new) }
-
-    subject { Radical::Router.new(routes).route([[:get, :user, 1]])[:user][1] }
-
+    let(:routes) { [user_profile_route, user_cart_route].map(&:new) }
     it { is_expected.to include(profile: a_hash_including(name: 'Luke')) }
     it { is_expected.to include(cart: a_hash_including(line_items: be_a(Array))) }
   end
