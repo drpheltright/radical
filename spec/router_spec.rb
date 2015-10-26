9adf98767b69c3da6e972a9d4b6e2d8eeda84852
Luke Morton <lukemorton.dev@gmail.com>
2015-10-26 23:31:53 +0000

    Add example of when single request matches single route

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -1,13 +1,35 @@
 describe Radical::Router do
+  let(:profile_schema) { Radical::Typed::Hash[name: String] }
+  let(:line_items_schema) { Radical::Typed::Array[Radical::Typed::Hash[id: Integer]] }
+  let(:cart_schema) { Radical::Typed::Hash[line_items: line_items_schema] }
+  let(:user_schema) { Radical::Typed::Hash[profile: profile_schema, cart: cart_schema] }
+
   context 'when single request matches single route' do
+    let(:route_1) do
+      Radical::Route[:user, Radical::Arg[:id], user_schema].define do
+        def get(id)
+          { profile: { name: 'Luke' } }
+        end
+      end
+    end
+
+    let(:route_2) do
+      Radical::Route[:post, Radical::Arg[:id], Radical::Typed::Hash[title: String]].define do
+        def get(id)
+          { title: 'Very good post' }
+        end
+      end
+    end
+
+    let(:routes) { [route_1, route_2].map(&:new) }
+
+    subject { Radical::Router.new(routes).route([[:get, :user, 1]])[:user][1] }
+
+    it { is_expected.to include(profile: a_hash_including(name: 'Luke')) }
+    it { is_expected.to_not include(:post) }
   end
 
   context 'when single request matches two routes' do
-    let(:profile_schema) { Radical::Typed::Hash[name: String] }
-    let(:line_items_schema) { Radical::Typed::Array[Radical::Typed::Hash[id: Integer]] }
-    let(:cart_schema) { Radical::Typed::Hash[line_items: line_items_schema] }
-    let(:user_schema) { Radical::Typed::Hash[profile: profile_schema, cart: cart_schema] }
-
     let(:route_1) do
       Radical::Route[:user, Radical::Arg[:id], user_schema].define do
         def get(id)
