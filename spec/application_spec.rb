be26b8d7a5fe42dc59b7ea489ddc99a6cb560871
Luke Morton <lukemorton.dev@gmail.com>
2015-11-01 22:31:08 +0000

    Restructure examples

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -3,18 +3,18 @@ describe Radical::Application do
   let(:response) { app.call(request) }
 
   context 'when valid get request made' do
-    let(:request) { Rack::MockRequest.env_for('/', params: { route: { name: nil } }) }
-    let(:router) { double(route: { name: 'Luke' }) }
-    before(:each) { app.router = router }
-
-    context 'then the application' do
-      it 'should forward request to router with params' do
-        response
-        expect(router).to have_received(:route).with([[:get, :name]])
-      end
-    end
-
     context 'and a route matches request' do
+      let(:request) { Rack::MockRequest.env_for('/', params: { route: { name: nil } }) }
+      let(:router) { double(route: { name: 'Luke' }) }
+      before(:each) { app.router = router }
+
+      context 'then the application' do
+        it 'should forward request to router with params' do
+          response
+          expect(router).to have_received(:route).with([[:get, :name]])
+        end
+      end
+
       context 'then the response status' do
         subject { response.status }
         it { is_expected.to eq(200) }
@@ -23,6 +23,20 @@ describe Radical::Application do
       context 'then the response body' do
         subject { response.body.first }
         it { is_expected.to eq(JSON.dump(name: 'Luke')) }
+      end
+    end
+
+    context 'and no route matches' do
+      let(:request) { Rack::MockRequest.env_for('/', params: { route: { nothing: nil } }) }
+
+      context 'then the response status' do
+        subject { response.status }
+        it { is_expected.to eq(404) }
+      end
+
+      context 'then the response body' do
+        subject { JSON.parse(response.body.first) }
+        it { is_expected.to include('error' => a_kind_of(String)) }
       end
     end
   end
@@ -49,20 +63,6 @@ describe Radical::Application do
         subject { response.body.first }
         it { is_expected.to eq(JSON.dump(name: 'Luke')) }
       end
-    end
-  end
-
-  context 'when valid request made and no route matches' do
-    let(:request) { Rack::MockRequest.env_for('/', params: { route: { nothing: nil } }) }
-
-    context 'then the response status' do
-      subject { response.status }
-      it { is_expected.to eq(404) }
-    end
-
-    context 'then the response body' do
-      subject { JSON.parse(response.body.first) }
-      it { is_expected.to include('error' => a_kind_of(String)) }
     end
   end
 
